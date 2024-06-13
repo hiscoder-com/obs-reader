f92b778f05f23111d6b0897f97b67c4fa53f8f3c
Vitek Foxprogs <foxprogs@gmail.com>
2024-06-13 23:30:01 +0400

    fixed click block size

--- src/pages/loadFrom.jsx
+++ src/pages/loadFrom.jsx
@@ -17,22 +17,22 @@ export default function LoadFromPage() {
 
   return (
     <Block className="mt-5 mx-auto max-w-4xl">
-      <Card colors={{ bgIos: 'bg-figma-bg-card-light dark:bg-figma-bg-card-dark', bgMaterial: 'bg-figma-bg-card-light dark:bg-figma-bg-card-dark' }} raised className='flex items-center flex-col' header={<p className='text-lg'>{t('LoadFromInternet')}</p>}>
-        <Link to="/app/internet/" component={RouterLink}>
+      <Link to="/app/internet/" component={RouterLink} className='w-full'>
+        <Card colors={{ bgIos: 'bg-figma-bg-card-light dark:bg-figma-bg-card-dark', bgMaterial: 'bg-figma-bg-card-light dark:bg-figma-bg-card-dark' }} raised className='flex items-center flex-col w-full' header={<p className='text-lg'>{t('LoadFromInternet')}</p>}>
           <div className={`rounded-full border-2 w-18 h-18 flex justify-center items-center cursor-pointer border-primary text-primary`}>
             <Icon ios={<CloudDownload className="w-7 h-7" />}
               material={<MdCloudDownload className="w-7 h-7" />} />
           </div>
-        </Link>
-      </Card>
-      <Card colors={{ bgIos: 'bg-figma-bg-card-light dark:bg-figma-bg-card-dark', bgMaterial: 'bg-figma-bg-card-light dark:bg-figma-bg-card-dark' }} raised className='flex items-center flex-col' header={<p className='text-lg'>{t('LoadFromDevice')}</p>}>
-        <Link to="/app/device/" component={RouterLink}>
+        </Card>
+      </Link>
+      <Link to="/app/device/" component={RouterLink} className='w-full'>
+        <Card colors={{ bgIos: 'bg-figma-bg-card-light dark:bg-figma-bg-card-dark', bgMaterial: 'bg-figma-bg-card-light dark:bg-figma-bg-card-dark' }} raised className='flex items-center flex-col w-full' header={<p className='text-lg'>{t('LoadFromDevice')}</p>}>
           <div className={`rounded-full border-2 w-18 h-18 flex justify-center items-center cursor-pointer border-primary text-primary`}>
             <Icon ios={<Folder className="w-7 h-7" />}
               material={<MdFolderOpen className="w-7 h-7" />} />
           </div>
-        </Link>
-      </Card>
+        </Card>
+      </Link>
     </Block>
   );
 }
